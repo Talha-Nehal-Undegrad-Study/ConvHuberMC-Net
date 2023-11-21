aba7e53013de8d2305a40e627ae93e7104379a90
Talha Ahmed <24100033@lums.edu.pk>
2023-11-22 03:04:20 +0500

    update

--- python_scripts/hubermc.py
+++ python_scripts/hubermc.py
@@ -2,38 +2,28 @@ import numpy as np
 import torch
 from torch import nn
 from torch.autograd import Variable
+import scipy.stats as stats
+import concurrent.futures
 # Function for cpu or gpu assignment
 
 def to_var(X, CalInGPU):
     if CalInGPU and torch.cuda.is_available():
         X = X.cuda()
     return Variable(X)
-
-def get_row_col_indices(matrix):
-    # Get dimensions of matrix
-    n1, n2 = matrix.shape
     
-    # First Column wise
-    indices_col = [np.where(matrix[:, i] == 1)[0] for i in range(n2)]
-
-    # Now Row wise
-    indices_row = [np.where(matrix[i, :] == 1)[0] for i in range(n1)]
-
-    return indices_col, indices_row
-    
-class HuberCell(nn.Module):
+class HuberCellV(nn.Module):
     # Constructor initalizes all the parameters that were passed to it from the unfolded net. Note: v, neta, lamda1/2, S are different for each layer. coef_gamma is constant
-    def __init__(self, c, lamda, mu, delta, tau, CalInGPU, layer):
-        super(HuberCell,self).__init__()
-        
-        self.layer = layer
+    def __init__(self, c, w, lamda, sigma, mu, delta, tau, iter, CalInGPU = True):
+        super(HuberCellV,self).__init__()
 
-        self.c = (c)
-        self.lamda = (lamda)
-        self.mu = (mu)
-
-        self.delta = (delta)
-        self.tau = (tau)
+        self.c = nn.Parameter(c)
+        self.w = nn.Parameter(w)
+        self.lamda = nn.Parameter(lamda)
+        self.sigma = nn.Parameter(sigma)
+        self.mu = nn.Parameter(mu)
+        self.delta = nn.Parameter(delta)
+        self.tau = nn.Parameter(tau)
+        self.iter = iter
         
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         
@@ -41,55 +31,152 @@ class HuberCell(nn.Module):
         self.relu = nn.ReLU()
         self.sig = nn.Sigmoid()
 
+    
+    def get_row_col_indices(matrix): # returns the indices of the sampling matrix where row entries or column entries are non-zero
+    # Get dimensions of matrix
+        n1, n2 = matrix.shape
+        
+        # First Column wise
+        indices_col = [torch.where(matrix[:, i] == 1)[0] for i in range(n2)]
+        indices_col = [tensor.tolist() for tensor in indices_col]
 
+        # Now Row wise
+        indices_row = [np.where(matrix[i, :] == 1)[0] for i in range(n1)]
+        indices_row = [tensor.tolist() for tensor in indices_row]
+
+        return indices_col, indices_row
 
     # Forward Pass recieves a list of 3 elements, data tensor of shape (2, 160, 320), a mask of shape (160, 320) which is True wherever there is
     # missing value in the matrix at index data[0], and rank of lowrank matrix
+    
+    def get_filter_mat(self, matrix, U, indices_col, col_num):
+        lst = indices_col[col_num]
+        mat_filter = torch.tensor([matrix[sub, col_num] for sub in lst]).unsqueeze(1)
+        u_filter = ([U[sub, :] for sub in lst])
+        u_filter = torch.stack(u_filter, dim = 1).T
+        return mat_filter, u_filter
+    
+    def score_func(self, x):
+        return torch.where(torch.abs(x) <= self.c, x, self.c * torch.sign(x))
+    
+    def weighted_dot_product(self, weight_vec, a_vec, b_vec):
+        return torch.sum(weight_vec * a_vec * b_vec)
 
     def forward(self, lst):
-        X_Omega, smapling_mat, rank = lst[0], lst[1], lst[2]
-        r, c = X_Omega.shape
-        self.U = torch.rand((r, rank)) 
-        self.V = U.torch.rand((rank, c))
+        X_Omega, sampling_mat, rank, U, v_col, col_num = lst[0], lst[1], lst[2], lst[3], lst[4], lst[5]
+        indices_col, indices_row = self.get_row_col_indices(sampling_mat)
+        n1, n2 = X_Omega.shape
 
-        # Get the lowrank and the L matrix which is for now (49, 60) zeros
-        input = data[0]
-        L = data[1]
+        # Get alpha for Fischer Consistency
+        alpha = (self.c ** 2 / 2) * (1 - stats.chi2.pdf(self.c ** 2, 1)) + (1 / 2) * stats.chi2.pdf(self.c ** 2, 3)
+        
+        # Get U_Ij Dagger
+        mat_filter, U_filter = self.get_filter_mat(matrix = X_Omega, U = U, indices_col = indices_col, col_num = col_num)
+        U_dagger = np.linalg.inv(U_filter.T @ U_filter) @ U_filter.T
 
-        # get the dimension and the parameters
-        H, U = input.shape
-        th_incomplete_tau = self.sig(self.v) * self.coef_gamma
-        th_neta = self.neta
-        th_lamda1 =  self.lamda1
-        th_lamda2 =  self.lamda2
-        th_rho = self.rho
-        th_S = self.S
-        coef_gamma = self.coef_gamma
+        # Apply Hubreg Algorithm for as many as there are iterations.
+        for _ in range(0, self.iter):
+            # Get Residual Vector
+            res_vec = mat_filter - np.dot(U_filter, v_col)
+            # Update Step size for scale
+            self.lamda = self.lamda + torch.log(torch.norm(self.score_func((res_vec / (self.sigma * (self.tau ** self.lamda))) * (1 / torch.sqrt(2 * alpha * len(U_filter)))), p = 2))
+            # Updated Scale
+            self.sigma = self.sigma * (self.tau ** self.lamda)
+            # Update Direction for update vj - delta
+            self.delta = np.dot(U_dagger, self.score_func(res_vec / self.sigma) * self.sigma)
+            # Get Regression Z = Xdelta
+            z = np.dot(U_filter, self.delta)
+            # Update step size for vj --> mu by first finding appropriate weighting throught IRWLS
+            w_vec = self.w * ((res_vec - self.mu * z) / self.sigma)
+            self.mu = self.weighted_dot_product(w_vec, z, z) * self.weighted_dot_product(w_vec, res_vec, z)
+            # Update Vj
+            v_col = v_col + self.mu * self.delta
 
-        # Get Q_tilda which is the diagonal of the flattened Q - sampling matrix
-        Q = entries_mask.view(H, U).float()
-        Q_tilda = torch.diag(Q.view(Q.shape[0] * Q.shape[1]))
+        return v_col
+    
 
-        # Get S_tilda which is the kronecker product of I_N identity matrix (where N is 60) and S*S
-        S_tilda = torch.kron(torch.eye(U, device = torch.device(self.device), requires_grad = self.CalInGPU), torch.matmul(th_S.T, th_S))
+class HuberCellU(nn.Module):
+    # Constructor initalizes all the parameters that were passed to it from the unfolded net. Note: v, neta, lamda1/2, S are different for each layer. coef_gamma is constant
+    def __init__(self, c, w, lamda, sigma, mu, delta, tau, iter, CalInGPU = True):
+        super(HuberCellV,self).__init__()
 
-        # The Reconstruction Layer - Eq (12 in original paper)
-        # Second half of equation - L is Z in the paper
-        vec = th_rho * (L.view(H, U) - th_P.view(H, U)) + torch.where(entries_mask.view(H, U), input.view(H, U), 0)
-        # First half of equation
-        Xtmp1 = torch.linalg.inv(Q_tilda + th_lamda1 * D_tilda + th_lamda2 * S_tilda + th_rho * torch.eye(H * U, device = torch.device(self.device), requires_grad = self.CalInGPU))
-        # Combining the two
-        Xtmp = Xtmp1 @ vec.view(H * U)
+        self.c = nn.Parameter(c)
+        self.w = nn.Parameter(w)
+        self.lamda = nn.Parameter(lamda)
+        self.sigma = nn.Parameter(sigma)
+        self.mu = nn.Parameter(mu)
+        self.delta = nn.Parameter(delta)
+        self.tau = nn.Parameter(tau)
+        self.iter = iter
+        
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        
+        self.CalInGPU = CalInGPU
+        self.relu = nn.ReLU()
+        self.sig = nn.Sigmoid()
 
-        # The Non-Linear Transform Layer - Eq (13 in original paper)
-        Ltmp = self.svtC(Xtmp.view(H, U) + th_P.view(H, U), th_incomplete_tau)
+    
+    def get_row_col_indices(matrix): # returns the indices of the sampling matrix where row entries or column entries are non-zero
+    # Get dimensions of matrix
+        n1, n2 = matrix.shape
+        
+        # First Column wise
+        indices_col = [torch.where(matrix[:, i] == 1)[0] for i in range(n2)]
+        indices_col = [tensor.tolist() for tensor in indices_col]
 
-        # The Multiplier Update Layer - Eq (14 in original paper)
-        Ptmp = th_P.view(H, U) + th_neta * (Xtmp.view(H, U) - Ltmp.view(H, U))
+        # Now Row wise
+        indices_row = [np.where(matrix[i, :] == 1)[0] for i in range(n1)]
+        indices_row = [tensor.tolist() for tensor in indices_row]
 
-        # Update data tensor with the reconstructed prediction and return the list. The list gets passed to the next ISTA cell and then after all layers returns the list.
-        data[1] = Ltmp.view(H, U)
-        return [data, entries_mask, D_tilda, Ptmp.view(H,U)]
+        return indices_col, indices_row
+
+    # Forward Pass recieves a list of 3 elements, data tensor of shape (2, 160, 320), a mask of shape (160, 320) which is True wherever there is
+    # missing value in the matrix at index data[0], and rank of lowrank matrix
+    
+    def get_filter_mat(self, matrix, V, indices_row, row_num):
+        lst = indices_row[row_num]
+        mat_filter = torch.tensor([matrix[row_num, sub] for sub in lst]).unsqueeze(1).T
+        v_filter = ([V[:, sub] for sub in lst])
+        v_filter = torch.stack(v_filter, dim = 1)
+        return mat_filter, v_filter
+    
+    def score_func(self, x):
+        return torch.where(torch.abs(x) <= self.c, x, self.c * torch.sign(x))
+    
+    def weighted_dot_product(self, weight_vec, a_vec, b_vec):
+        return torch.sum(weight_vec * a_vec * b_vec)
+
+    def forward(self, lst):
+        X_Omega, sampling_mat, rank, V, u_row, row_num = lst[0], lst[1], lst[2], lst[3], lst[4], lst[5]
+        indices_col, indices_row = self.get_row_col_indices(sampling_mat)
+        n1, n2 = X_Omega.shape
+
+        # Get alpha for Fischer Consistency
+        alpha = (self.c ** 2 / 2) * (1 - stats.chi2.pdf(self.c ** 2, 1)) + (1 / 2) * stats.chi2.pdf(self.c ** 2, 3)
+        
+        # Get U_Ij Dagger
+        mat_filter, v_filter = self.get_filter_mat(matrix = X_Omega, V = V, indices_col = indices_row, col_num = row_num)
+        v_dagger = np.linalg.inv(v_filter.T @ v_filter) @ v_filter.T
+
+        # Apply Hubreg Algorithm for as many as there are iterations.
+        for _ in range(0, self.iter):
+            # Get Residual Vector
+            res_vec = (mat_filter) - np.dot(u_row, v_filter)
+            # Update Step size for scale
+            self.lamda = self.lamda + torch.log(torch.norm(self.score_func((res_vec / (self.sigma * (self.tau ** self.lamda))) * (1 / torch.sqrt(2 * alpha * len(v_filter)))), p = 2))
+            # Updated Scale
+            self.sigma = self.sigma * (self.tau ** self.lamda)
+            # Update Direction for update vj - delta
+            self.delta = np.dot(self.score_func(res_vec / self.sigma) * self.sigma, v_dagger) # Opposite
+            # Get Regression Z = Xdelta
+            z = np.dot(self.delta, v_filter) # Opposite
+            # Update step size for vj --> mu by first finding appropriate weighting throught IRWLS
+            w_vec = self.w * ((res_vec - self.mu * z) / self.sigma)
+            self.mu = self.weighted_dot_product(w_vec, z, z) * self.weighted_dot_product(w_vec, res_vec, z)
+            # Update Vj
+            u_row = u_row + self.mu * self.delta
+
+        return u_row
 
 class UnfoldedNet_Huber(nn.Module):
     def __init__(self, params = None, model_denoise = None):
@@ -98,15 +185,21 @@ class UnfoldedNet_Huber(nn.Module):
         # Constructor initializes various parameters from the given parameter dictionary
         self.layers = params['layers']
         self.CalInGPU = params['CalInGPU']
-        
+
         self.n1, self.n2 = params['size1'], params['size2']
         self.rank = params['rank']
-        
-        self.model_denoise = model_denoise
+        self.iter = params['iter']
+
+        self.U = torch.ones(self.n1, self.rank)
+        self.V = torch.ones(self.rank, self.n2)        
+        # self.model_denoise = model_denoise
 
         self.rank = params['rank']
 
+        # Learning parameters for hubreg per layer
+
         self.c = to_var(torch.ones(self.layers, requires_grad = True) * params['initial_c'], self.CalInGPU)
+        self.w = to_var(torch.ones(self.layers, requires_grad = True) * params['initial_w'], self.CalInGPU)
         self.initial_lamda = to_var(torch.ones(self.layers, requires_grad = True) * params['initial_lamda'], self.CalInGPU)
         self.initial_sigma = to_var(torch.ones(self.layers, requires_grad = True) * params['initial_sigma'], self.CalInGPU)
         self.initial_mu = to_var(torch.ones(self.layers, requires_grad = True) * params['initial_mu'], self.CalInGPU)
@@ -121,45 +214,45 @@ class UnfoldedNet_Huber(nn.Module):
         self.sig = nn.Sigmoid()
         self.relu = nn.ReLU()
 
-        self.model = self.makelayers()
+        self.indices_col, self.indices_row = self.get_row_col_indices()
+        self.init_model = self.makelayers()
 
     # Function which intializes num_layers ISTA cells by passing it those parameters that are learnt per layer like lambda1/2, neta, S and those that are fixed like coef_gamma
     def makelayers(self):
         filt = []
-        for layer_idx in range(1, self.layers + 1):
-          if layer_idx % 2 != 0: # --> V Update Layer
-              
-          pass
-
+        for layer_idx in range(self.layers):
+            if (layer_idx + 1) % 2 != 0: # --> V Update Layer - n2 nodes
+                layer_nodes = nn.ModuleList([HuberCellV(self.c[layer_idx], self.w[layer_idx], self.initial_lamda[layer_idx], 
+                                                        self.initial_sigma[layer_idx], self.initial_mu[layer_idx], 
+                                                        self.initial_delta[layer_idx], self.initial_tau[layer_idx], self.iter) 
+                                                        for col in range(self.n2)])
+                filt.append(layer_nodes)
+            else: # --> U Update Layer - n1 nodes
+                layer_nodes = nn.ModuleList([HuberCellU(self.c[layer_idx], self.w[layer_idx], self.initial_lamda[layer_idx], 
+                                                        self.initial_sigma[layer_idx], self.initial_mu[layer_idx], 
+                                                        self.initial_delta[layer_idx], self.initial_tau[layer_idx], self.iter) 
+                                                        for col in range(self.n1)])
+                filt.append(layer_nodes)
+        return nn.Sequential(*filt)
     # Forward Pass recieves the lowrank noisy matrix
     def forward(self, x):
-                
-        self.U = torch.ones(self.n1, self.rank)
-        self.V = torch.ones(self.rank, self.n2)
 
         # Now initalize the neural architecture of even number of layers where even numbered layers correspond to updating U and odd numbered
         # layers correspond to updating V. Each odd numbered layers will have number of nodes equal to number of rows and each even numbered 
         # column will have number of nodes equal to the number of columns. Each node in layer is connected to a huber cell of certain number of
         # iterations and each huber cell in each layer will have same initalization of parameters at start. 
 
-        # We intialize a tensor 'data' of shape (2, 160, 320) and assign the first index of it with the lowrank component passed i.e. first element of x
-        data = to_var(torch.zeros([2] + list(x[0].shape)), self.CalInGPU)
-        data[0] = x[0]
+        sampling_mat = torch.where(x != 0, torch.tensor(1), torch.tensor(0))
+        lst = [x, sampling_mat, self.rank, ] #  X_Omega, sampling_mat, rank, V, u_row, row_num send these arguments some to huber cell
+        # but the problem is the model is sequential and different arguments for V or U Huber Cell. So use multiprocessing here if possible
 
-        # Get the dimensions i.e. H = 160, U = 320 and then intialize a sampling matrix of shape (160, 320) which is True wherever there is a missing value in x/L
-        H, U = x[0].shape
-        entries_mask = (torch.isnan(data[0]))
-        # entries_mask = (data[0] != 0)
-        
-        # Pass the lowrank component to the denoised_autoencode model
-        x_reconstructed = self.model_denoise.forward(x_reconstructed)
-        
-        data[0] = x_reconstructed
-
-        # Then forward pass to the Huber cell, the data tensor, the mask rank
-        ans = self.filter([data, entries_mask, self.rank]) # X_Omega --> X_tilda --> UV_tilda --> 
-        data = ans[0] 
-        return ans
+        # Basic syntax
+        with concurrent.futures.ProcessPoolExecutor() as executor:
+            # Define a list of arguments here for e.g a list of u_rows or v_cols and somehow pass it to repsective huber cell. Rest is done hopefully
+            # check this link for more details: https://arnabocean.com/frontposts/2020-10-01-python-multicore-parallel-processing/
+        return None
+    
+    # Ignore the below function for now.
 
     def getexp_LS(self):
         neta = self.neta
