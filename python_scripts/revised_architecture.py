0d5a5786037b2fbed71ec3bc4ca7e3576726a78c
Talha Ahmed <24100033@lums.edu.pk>
2024-03-21 00:30:56 +0500

    update

--- python_scripts/revised_architecture.py
+++ python_scripts/revised_architecture.py
@@ -5,46 +5,48 @@ from torch.autograd import Variable
 import scipy.stats as stats
 import concurrent.futures
 
+from python_scripts.utils import psi, rho, alpha
+
 def to_var(X, CalInGPU):
     if CalInGPU and torch.cuda.is_available():
         X = X.to('cuda')
     else:
         X = X.to('cpu')
     return Variable(X)
+    
+class Conv2dC(nn.Module):
+    def __init__(self, kernel): # Empirically found to be self.kernel to maintain same shape
+        super(Conv2dC, self).__init__()
+
+        # Given a kernel size of 2 dimensions, we calculate the padding through the formula (k[0] - 1)/2 --> this helps maintain the shape as close as possible
+        pad0 = int((kernel[0] - 1) / 2)
+        pad1 = int((kernel[1] - 1) / 2)
+        if torch.cuda.is_available():
+            self.convR = nn.Conv2d(1, 1, (kernel[0], kernel[0]), (1, 1), (pad0, pad0), groups = 1).cuda()
+        else:
+            self.convR = nn.Conv2d(1, 1, (kernel[0], kernel[0]), (1, 1), (pad0, pad0), groups = 1).to('cpu')
+        # At groups = in_channels, each input channel is convolved with its own set of filters (of size out_channels/in_channels)
+
+    def forward(self, x):
+        # get the height dimension and convert it to int
+        n = x.shape[-1]
+        # This line creates a new tensor xR by slicing the input tensor along the columns dimension (0:n). The None adds an extra dimension, making xR a 4-dimensional tensor with size (1, 1, H, W).
+        # The 1's make sure the consistency with the conv operation which is expecting a in_channels, out_channels, which are set to 1
+        xR = x[None, None, :, 0:n].clone()
+        xR = self.convR(xR)
+        # Removing the extra dimension
+        xR = xR.squeeze()
+        x = xR
+        return x
+
 
 class Huber(nn.Module):
-    def __init__(self, input_size, iter):
+    def __init__(self, kernel, conv_layers, iter):
         super(Huber, self).__init__()
 
         self.hubreg_iters = iter
-        self.conv_op = nn.Conv1d(in_channels = input_size, 
-                                 out_channels = input_size, 
-                                 kernel_size = 1, 
-                                 stride = 1, 
-                                 padding = 0)
-        self.sigma = sigma
-        
-        self.c = nn.Parameter(c)
-        # print(f'self.c: {self.c}')
-        self.lamda = nn.Parameter(lamda)
-        # print(f'self.lamda: {self.lamda}')
-        self.mu = nn.Parameter(mu)
-        # print(f'self.mu: {self.mu}')
-        
-        # self.c_list = nn.Parameter(torch.tensor(self.layers * [params['initial_c']]).to(self.device))
-        # print(self.c_list)
-        # self.lamda_list = nn.Parameter(torch.tensor(self.layers * [params['initial_lamda']]).to(self.device))
-        # self.mu_list = nn.Parameter(torch.tensor(self.layers * [params['initial_mu']]).to(self.device))
-
-        # learnables
-        # self.c_list = nn.ParameterList([nn.Parameter(c) for c in c_list])
-        # self.lamda_list = nn.ParameterList([nn.Parameter(lamda) for lamda in lamda_list])
-        # self.mu_list = nn.ParameterList([nn.Parameter(mu) for mu in mu_list])
-
-        # non-learnables
-        # self.sigma = sigma
-        # self.hubreg_iters = hubreg_iters
-        # self.layers = layers
+        self.conv_layers = conv_layers
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
     def get_rows(self, column):
         # returns row indices of non-zero elements in column
@@ -61,92 +63,65 @@ class Huber(nn.Module):
     def hubregv(self, tup_arg):
         # beta: (r, 1), X: (j_i, r), y: (j_i, 1)
 
-        beta, X, y = tup_arg[0], tup_arg[1], tup_arg[2]
+        beta, X, y, conv_op = tup_arg[0], tup_arg[1], tup_arg[2], tup_arg[3]
+        gamma = 2
+        
+        X_plus = torch.linalg.pinv(X) # (r, j_i)
+        X_plus_conv = conv_op(X_plus)
 
-        sigma = 0.67
-        # print(f'in V: beta.shape: {beta.shape}, X.shape: {X.shape}, y.shape: {y.shape}')
 
-        # print(sigma.requires_grad)
-        # c = self.c.clone().cpu().detach()
-        # alpha = ((0.5 * (c * 2) * (1 - stats.chi2.cdf(c * 2, df = 1))) + (0.5 * stats.chi2.cdf(c ** 2, df = 3)))
-        alpha = 0.32933136656447193
+        # For inital estimate beta: might consider nearest neighbour filling thingy
 
-        try:
-            X_plus = torch.linalg.pinv(X)
-        except Exception as e:
-            print(e)
-            # print(X.shape, X.t().shape)
-            # print(X, X.t(), X.t() @ X, sep = '\n')
-            # temp = X.t() @ X
-            # for i in range(len(temp)):
-            #     print(i)
-            #     print(temp[i, i])
-            return None
-        # print(X_plus.requires_grad)
+        r = y - torch.mm(X, beta) # y - XBeta
+        scale = 1.4815 * torch.median(torch.abs(r - torch.median(r)))
+
+        N = y.shape[0]
+        p = beta.shape[0]
+
         for _ in range(self.hubreg_iters):
-            
-            dummy_beta = beta.clone()
-            r = y - (torch.matmul(X, dummy_beta))
-            # print(r.requires_grad)
-            tau = torch.norm(self.hub_deriv(r / sigma)) / ((2 * len(y) * alpha) ** 0.5)
 
-            sigma = tau ** self.lamda
+            r = y - torch.mm(X, beta)
+            r_chi = psi(r / scale) * (r / scale) - rho(r / scale)
+            scale = torch.sqrt(((gamma * scale ** 2) / (2 * alpha() * (N - p - 1))) * torch.sum(r_chi))
 
-            # print(sigma.requires_grad)
-            delta = X_plus @ (self.hub_deriv(r / sigma) * sigma)
-            # delta = 1.2
-            # print(delta.requires_grad)
-            beta = dummy_beta + (self.mu * delta)
-            # print(beta.requires_grad)
+            r_pseu = psi(r / scale) * scale
 
-        # Return the result and attach gradients
-        # print(f'Learnable c: {self.c_list[layer].requires_grad}, Learnable lamda: {self.lamda_list[layer].requires_grad}, Learnable mu: {self.mu_list[layer].requires_grad}')
+            delta = torch.mm(X_plus_conv, r_pseu)
+
+            beta = beta + delta
+
         return beta
-        # return beta
+
     
     def hubregu(self, tup_arg):
         # beta: (1, r), X: (r, i_j), y: (1, i_j)
 
-        beta, X, y = tup_arg[0], tup_arg[1], tup_arg[2]
-        # print(f'in U: beta.shape: {beta.shape}, X.shape: {X.shape}, y.shape: {y.shape}')
+        beta, X, y, conv_op = tup_arg[0], tup_arg[1], tup_arg[2], tup_arg[3]
+        gamma = 2
 
-        # Detach parameters before using them in the function
-        # c = self.c.clone().cpu().detach()
-        # alpha = ((0.5 * (c * 2) * (1 - stats.chi2.cdf(c * 2, df = 1))) + (0.5 * stats.chi2.cdf(c ** 2, df = 3)))
-        alpha = 0.32933136656447193
-        sigma = 0.67
-
-        try:
-            X_plus = torch.linalg.pinv(X)
-            
-        except Exception as e:
-            print(e)
-            # print(X.shape, X.t().shape)
-            # temp = X.t() @ X
-            # for i in range(len(temp)):
-            #     print(i)
-            #     print(temp[i, i])
-            return None
+        X_plus = torch.linalg.pinv(X) # (i_j, r)
+        X_plus_conv = conv_op(X_plus)
         
+        r = y - torch.mm(beta, X) # y - XBeta
+        scale = 1.4815 * torch.median(torch.abs(r - torch.median(r)))
+
+        N = y.shape[1]
+        p = beta.shape[1]
+
         for _ in range(self.hubreg_iters):
 
-            dummy_beta = beta.clone()            
-            r = y - (torch.matmul(dummy_beta, X)) # (1, j_i)
-            
-            tau = torch.norm(self.hub_deriv(r / sigma)) / ((2 * len(y) * alpha)**0.5)
-            
-            sigma = tau ** self.lamda
-            
-            delta = (self.hub_deriv(r / sigma) * sigma) @ X_plus
-            # delta = 1.2
-            beta = dummy_beta + (self.mu * delta) # (1, r)
-            
+            r = y - torch.mm(beta, X)
+            r_chi = psi(r / scale) * (r / scale) - rho(r / scale)
+            scale = torch.sqrt(((gamma * scale ** 2) / (2 * alpha() * (N - p - 1))) * torch.sum(r_chi))
 
-        # Return the result and attach gradients
-        # print(f'Learnable c: {self.c_list[layer].requires_grad}, Learnable lamda: {self.lamda_list[layer].requires_grad}, Learnable mu: {self.mu_list[layer].requires_grad}')
-        
+            r_pseu = psi(r / scale) * scale
+
+            delta = torch.mm(r_pseu, X_plus_conv)
+
+            beta = beta + delta
+
         return beta
-        # return beta
+
 
     def forward(self, lst):
         # runs Algorithm 1 from Robust M-Estimation Based Matrix Completion
@@ -161,11 +136,11 @@ class Huber(nn.Module):
         # for layer in range(self.layers):
         for j in range(V.shape[1]):
             rows = self.get_rows(X[:, j]) # row indices for jth column
-            V[:, j: j + 1] = self.hubregv((V[:, j: j + 1], U[rows, :], X[rows, j: j + 1]))
+            V[:, j: j + 1] = self.hubregv((V[:, j: j + 1], U[rows, :], X[rows, j: j + 1], self.conv_layers[j]))
 
         for i in range(U.shape[0]):
             columns = self.get_rows(X[i, :]) # column indices for ith row
-            U[i: i + 1, :] = self.hubregu((U[i: i + 1, :], V[:, columns], X[i: i + 1, columns]))
+            U[i: i + 1, :] = self.hubregu((U[i: i + 1, :], V[:, columns], X[i: i + 1, columns], self.conv_layers[j + i]))
         return [X, U, V]
 
         # # for layer in range(self.layers):
@@ -193,25 +168,23 @@ class UnfoldedNet_Huber(nn.Module):
         self.U = torch.randn(self.n1, self.rank).to(self.device)
         self.V = torch.randn(self.rank, self.n2).to(self.device)
         
-        # self.model_denoise = model_denoise
+        self.kernel = params['kernel']
+        
+        # Create lists of Conv2dC instances
+        conv_layers_v = [Conv2dC(kernel = self.kernel) for _ in range(self.V.shape[1])]
+        conv_layers_u = [Conv2dC(kernel = self.kernel) for _ in range(self.U.shape[0])]
 
-        # self.c = to_var((torch.ones(self.layers) * torch.tensor(params['initial_c'])), self.CalInGPU)
-        # self.lamda = to_var(torch.ones(self.layers) * torch.tensor(params['initial_lamda']), self.CalInGPU)
-        # self.mu = to_var(torch.ones(self.layers) * torch.tensor(params['initial_mu']), self.CalInGPU)
+        # Concatenate the two lists
+        combined_conv_layers = conv_layers_v + conv_layers_u
 
-        self.c = (torch.ones(self.layers) * torch.tensor(params['initial_c'])).to(self.device)
-        # print(f'unfoldedc: {self.c}')
-        self.lamda = (torch.ones(self.layers) * torch.tensor(params['initial_lamda'])).to(self.device)
-        self.mu = (torch.ones(self.layers) * torch.tensor(params['initial_mu'])).to(self.device)
-
-        self.sigma = torch.tensor(params['initial_sigma']).to(self.device)
+        # Create a single ModuleList from the combined list
+        self.conv_layers = nn.ModuleList(combined_conv_layers)
+        print(len(self.conv_layers))
 
         filt = []
         for i in range(self.layers):
-            if i == 0:
-                filt.append(Huber(self.sigma, self.c[i], self.lamda[i], self.mu[i], self.iter))
-            else:
-                filt.append(Huber(self.sigma, self.c[i], self.lamda[i], self.mu[i], self.iter))
+            filt.append(Huber(self.kernel, self.conv_layers, self.iter))
+            
         self.huber_obj = nn.Sequential(*filt)
 
 
--- python_scripts/utils.py
+++ python_scripts/utils.py
@@ -10,12 +10,12 @@ def get_nonzeros(matrix):
 def rho(x):
     x = torch.as_tensor(x)
     subset = torch.abs(x) <= c
-    return 0.5 * x**2 * subset + (1 - subset) * (c * torch.abs(x) - 0.5 * c**2)
+    return 0.5 * x**2 * subset + (~subset) * (c * torch.abs(x) - 0.5 * c**2)
 
 def psi(x):
     x = torch.as_tensor(x)
     subset = torch.abs(x) <= c
-    return subset * x + (1 - subset) * c * torch.sign(x)
+    return subset * x + (~subset) * c * torch.sign(x)
 
 def alpha():
     return 0.7102
